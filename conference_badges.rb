a9fa4a937bc2d18bdc0d2cb8959571eca700f0e4
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-05 09:01:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  list = []
+ attendees.each do |name|
+   list << badge_maker(name)
+ end
+ list
+end
+
+def assign_rooms(attendees)
+  list = []
+  attendees.each_with_index do |name, index|
+  index += 1
+  list << "Hello, #{name}! You'll be assigned to room #{index}!"
+end
+list
+end
+
